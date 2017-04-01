5b024907dd8a8d89170e5941a0dd63ec9633bea7
damonMPR <woolpack.cwz@gmail.com>
2017-04-01 16:36:42 +0800

    添加javavc支持

---
+++ src/main/java/com/crimps/javavc/JavavcCamera.java
@@ -0,0 +1,13 @@
+/**
+ * @(#)com.crimps.javavc.JavavcCamera.java Copyright (c) 2014-2018 crimps
+ */
+package com.crimps.javavc;
+
+/**
+ *
+ * @author crimps
+ * @version 1.0  2017/3/31
+ * @modified crimps  2017/3/31  <创建>
+ */
+public class JavavcCamera {
+}
---
+++ src/test/java/com/crimps/javavc/JavavcCameraTest.java
@@ -0,0 +1,11 @@
+package com.crimps.javavc;
+
+import static org.junit.Assert.*;
+
+public class JavavcCameraTest {
+
+    @org.junit.Test
+    public void recordCamera() throws Exception {
+
+    }
+}
